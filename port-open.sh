5cbc176b84bf1dac96edd4239ee36baffe847940
Derek Forrest <dforrest@demandware.com>
2017-02-13 11:41:17 -0500

    G option not supported

--- port-open.sh
+++ port-open.sh
@@ -2,11 +2,11 @@
 
 HOST=$1
 PORT=$2
-touch test.txt
+
 echo "Trying to connect to port $PORT on $HOST"
 
 # Make sure the port is open
-nc -z -G 5 -w 5 "${HOST}" $PORT
+nc -z -w5 $HOST $PORT
 if [ $? -gt 0 ]; then   
     echo -e "failed to connect to ${HOST} on port ${PORT}"
     exit 1
